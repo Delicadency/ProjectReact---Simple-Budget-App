f3eb666db801089c5bb175ffc62e523c8ede7811
Agata Kamińska <a.fluffypie@gmail.com>
2024-08-27 12:57:34 +0200

    feat: created and imported Main component

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,7 @@
+import "./Main.css";
+
+const Main = () => {
+  return <main className="flex main f--column f--row"></main>;
+};
+
+export default Main;
--- src/components/ProjectContainer/ProjectContainer.jsx
+++ src/components/ProjectContainer/ProjectContainer.jsx
@@ -1,9 +1,11 @@
 import Header from "../Header/Header";
+import Main from "../Main/Main";
 
 const ProjectContainer = () => {
   return (
     <div className="flex f--wrap f--column">
       <Header />
+      <Main />
     </div>
   );
 };
