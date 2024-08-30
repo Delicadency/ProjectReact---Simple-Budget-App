6d059dba1aa3b339749da78c27f04927a71bd1e0
Agata Kamińska <a.fluffypie@gmail.com>
2024-08-30 12:16:32 +0200

    fix: section implementations corrected, moved to correct component

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,11 +1,16 @@
 import "./Main.css";
 import Section from "../Section/Section";
+import { TypeProvider } from "../TypeContext/TypeContext";
 
 const Main = () => {
   return (
     <main className="flex main f--column f--row">
-      <Section type={"income"} />
-      <Section type={"expense"} />
+    <TypeProvider type="income">
+        <Section />
+      </TypeProvider>
+      <TypeProvider type="expense">
+        <Section />
+      </TypeProvider>
     </main>
   );
 };
--- src/components/ProjectContainer/ProjectContainer.jsx
+++ src/components/ProjectContainer/ProjectContainer.jsx
@@ -1,19 +1,11 @@
 import Header from "../Header/Header";
 import Main from "../Main/Main";
-import Section from "../Section/Section";
-import { TypeProvider } from "../TypeContext/TypeContext";
 
 const ProjectContainer = () => {
   return (
     <div className="flex f--wrap f--column">
       <Header />
       <Main />
-      <TypeProvider type="income">
-        <Section />
-      </TypeProvider>
-      <TypeProvider type="expense">
-        <Section />
-      </TypeProvider>
     </div>
   );
 };
