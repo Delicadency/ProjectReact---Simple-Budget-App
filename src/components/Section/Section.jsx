7b206dc7f25039e227b3b252c51e52d0a7c71e66
Agata Kamińska <a.fluffypie@gmail.com>
2024-08-30 12:30:13 +0200

    feat: implemented useContext in Section Component

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,6 +1,13 @@
-const Section = ({ type }) => {
+import { useContext } from "react";
+import TypeContext from "../TypeContext/TypeContext";
+
+const Section = () => {
+  const type = useContext(TypeContext);
   return (
-    <section className="flex f--wrap a-i--center j-c--center" id={type}></section>
+    <section
+      className="flex f--wrap a-i--center j-c--center"
+      id={type}
+    ></section>
   );
 };
 
