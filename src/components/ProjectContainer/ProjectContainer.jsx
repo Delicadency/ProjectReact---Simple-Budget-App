475fb55b9de8c8fad85166d562200f7a3177bbb7
Agata Kamińska <a.fluffypie@gmail.com>
2024-08-30 11:53:50 +0200

    feat: imported Section component

--- src/components/ProjectContainer/ProjectContainer.jsx
+++ src/components/ProjectContainer/ProjectContainer.jsx
@@ -1,11 +1,14 @@
 import Header from "../Header/Header";
 import Main from "../Main/Main";
+import Section from "../Section/Section";
 
 const ProjectContainer = () => {
   return (
     <div className="flex f--wrap f--column">
       <Header />
       <Main />
+      <Section />
+      <Section />
     </div>
   );
 };
