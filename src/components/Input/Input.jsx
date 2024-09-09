3788d1f5b5b84b5e43c6bd719077b266e11dfcf9
Agata Kamińska <a.fluffypie@gmail.com>
2024-09-09 12:22:13 +0200

    fix: improved addition of attributes depending on prop value

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -8,20 +8,23 @@ const Input = ({ prop }) => {
   const amountAttributes =
     prop === "amount"
       ? {
+          palceholder: "Kwota",
           pattern: "^\\d{0,7}(\\.\\d{0,2})?$",
           inputMode: "numeric",
           step: "0.01",
+          maxLength: "10",
         }
-      : {};
+      : {
+          placeholder: "Nazwa wydatku",
+          maxLength: "30",
+        };
 
   return (
     <input
       className={`input input--${prop} input--${type} txt-a--center f-s-14"`}
       type={prop}
-      placeholder={prop === "text" ? "Nazwa wydatku" : "Kwota"}
       id={`${type}-${prop}`}
       autoComplete="off"
-      maxLength={prop === "text" ? "30" : "10"}
       {...amountAttributes}
     ></input>
   );
