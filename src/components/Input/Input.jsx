017b36b77a36493fc3b705073af4ab5b5e281fc0
Agata Kamińska <a.fluffypie@gmail.com>
2024-09-09 12:15:53 +0200

    fix: improved addition of attributes depending on prop value

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -4,6 +4,16 @@ import TypeContext from "../TypeContext/TypeContext";
 
 const Input = ({ prop }) => {
   const type = useContext(TypeContext);
+
+  const amountAttributes =
+    prop === "amount"
+      ? {
+          pattern: "^\\d{0,7}(\\.\\d{0,2})?$",
+          inputMode: "numeric",
+          step: "0.01",
+        }
+      : {};
+
   return (
     <input
       className={`input input--${prop} input--${type} txt-a--center f-s-14"`}
@@ -12,9 +22,7 @@ const Input = ({ prop }) => {
       id={`${type}-${prop}`}
       autoComplete="off"
       maxLength={prop === "text" ? "30" : "10"}
-      pattern={prop === "amount" ? "^d{0,7}(.d{0,2})?$" : ""}
-      inputMode={prop === "amount" ? "numeric" : ""}
-      step={prop === "amount" ? "0.01" : ""}
+      {...amountAttributes}
     ></input>
   );
 };
