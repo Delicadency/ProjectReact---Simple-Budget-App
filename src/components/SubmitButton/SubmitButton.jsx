89b0629b5585eb9c4f4f6c0a90397de2c85ba2dd
Agata Kamińska <a.fluffypie@gmail.com>
2024-09-09 12:39:53 +0200

    fix: improved addition of attributes depending on form button value

--- src/components/SubmitButton/SubmitButton.jsx
+++ src/components/SubmitButton/SubmitButton.jsx
@@ -4,17 +4,18 @@ import TypeContext from "../TypeContext/TypeContext";
 
 const SubmitButton = ({ isFormButton, listButtonType, text }) => {
   const type = useContext(TypeContext);
-  return (
-    <button
-      type="submit"
-      className={
-        isFormButton
-          ? `input__button input__button--${type} txt-a--center f-s-14`
-          : `list__button ${listButtonType} ${listButtonType}--${type}`
+  const buttonAtrributes = isFormButton
+    ? {
+        className: `input__button input__button--${type} txt-a--center f-s-14`,
+        id: `${type}-list`,
       }
-      id={isFormButton ? `${type}-list` : ""}
-    >
-      {isFormButton ?  text : ""}
+    : {
+        className: `list__button ${listButtonType} ${listButtonType}--${type}`,
+      };
+
+  return (
+    <button type="submit" {...buttonAtrributes}>
+      {isFormButton ? text : undefined}
     </button>
   );
 };
